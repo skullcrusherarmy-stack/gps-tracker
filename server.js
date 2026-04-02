fc0eae5bdfc498ec1ad6358fdbbc1bfb704f8dbe
Ishaan Pastariya <skullcrusherarmy@gmail.com>
2026-04-03 04:37:48 +0530

    fix port 8080

--- server.js
+++ server.js
@@ -58,8 +58,8 @@ app.get("/gps", async (req, res) => {
 });
 
 // ---------------- PORT (VERY IMPORTANT FOR RAILWAY) ----------------
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
